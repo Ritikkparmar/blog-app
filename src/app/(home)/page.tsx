e9d36abcb273e2e8f67d525f650dedb73added49
Ritik Parmar <ritikparmar255@gmail.com>
2025-04-12 09:57:31 +0530

    first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/ui/home/header/navbar";
+import HeroSection from "@/components/ui/home/hero-section";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      <HeroSection />
+    </div>
+  );
+}
