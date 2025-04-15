3befd422d95cb2d16038f9672e393a7666943fc6
Ritik Parmar <ritikparmar255@gmail.com>
2025-04-15 22:20:32 +0530

    adding create article page and server actions

---
+++ src/app/dashboard/articles/create/page.tsx
@@ -0,0 +1,14 @@
+
+import CreateArticlesPage from '@/components/articles/create-article-page'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <CreateArticlesPage/>
+    </div>
+    
+  )
+}
+
+export default  page
